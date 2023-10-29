6e9cb7efbbf83d5b6e6aa650324a4c584551d02e
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-10-30 01:50:38 +0800

    西元2023年10月30日 (週一) 01時50分38秒 CST

--- udocker.sh
+++ udocker.sh
@@ -5,10 +5,10 @@
 # - 如用到目錄，必須事前建立
 # - 目錄路徑必須是完整路徑
 
-PROJECT_NAME=docker-web-wiki
-IMAGE_NAME=pudding/docker-web:pwiki-20231029-0339
-LOCAL_VOLUMN_PATH=/var/www/html/
-LOCAL_PORT=80
+PROJECT_NAME=docker-web-Apache-Solr
+IMAGE_NAME=pudding/docker-web:docker-web-apache-solr-app-20231030-0146
+LOCAL_VOLUMN_PATH=/var/solr/data/collection/conf
+LOCAL_PORT=8983
 
 mkdir -p "/content/${PROJECT_NAME}"
 nohup udocker --allow-root run -p "8000:${LOCAL_PORT}" --volume="/content/${PROJECT_NAME}:${LOCAL_VOLUMN_PATH}" ${IMAGE_NAME} > .nohup.out 2>&1 &
