f32b9c7d62f88ff7b45e67b0c11a4bdd237fc8c3
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-10-31 13:04:44 +0800

    西元2023年10月31日 (週二) 13時04分44秒 CST

--- docker-build/udocker.sh
+++ docker-build/udocker.sh
@@ -9,7 +9,7 @@ PROJECT_NAME=docker-web-Apache-Solr
 IMAGE_NAME=pudding/docker-web:docker-web-apache-solr-app-20231031-0351
 LOCAL_VOLUMN_PATH=/var/solr/data/collection/conf
 LOCAL_PORT=8983
-RUN_IN_BACKGROUND=true
+RUN_IN_BACKGROUND=false
 
 echo "Image: ${IMAGE_NAME}"
 
@@ -18,7 +18,9 @@ mkdir -p "/content/${PROJECT_NAME}"
 udocker --allow-root rm $(udocker --allow-root ps -m -s | awk 'NR>1 {print $1}')
 
 if [ "$RUN_IN_BACKGROUND" = true ]; then
+    echo "Run container in background.."
     nohup udocker --allow-root run -p "8000:${LOCAL_PORT}" --volume="/content/${PROJECT_NAME}:${LOCAL_VOLUMN_PATH}" ${IMAGE_NAME} > .nohup.out 2>&1 &
 else
+    echo "Run container in foreground.."
     udocker --allow-root run -p "8000:${LOCAL_PORT}" --volume="/content/${PROJECT_NAME}:${LOCAL_VOLUMN_PATH}" ${IMAGE_NAME}
 fi
