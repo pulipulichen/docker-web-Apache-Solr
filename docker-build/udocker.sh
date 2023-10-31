3e433975ebc5314339fcc600fe89b7f4e6bd81f0
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-10-31 13:38:44 +0800

    西元2023年10月31日 (週二) 13時38分44秒 CST

--- docker-build/udocker.sh
+++ docker-build/udocker.sh
@@ -13,14 +13,14 @@ RUN_IN_BACKGROUND=true
 
 echo "Image: ${IMAGE_NAME}"
 
-mkdir -p "/content/${PROJECT_NAME}"
+mkdir -p "/content/docker-app/${PROJECT_NAME}"
 
 # udocker --allow-root rm $(udocker --allow-root ps -m -s | awk 'NR>1 {print $1}')
 
 if [ "$RUN_IN_BACKGROUND" = true ]; then
     echo "Run container in background.."
-    nohup udocker --allow-root run -p "${LOCAL_PORT}:${LOCAL_PORT}" --volume="/content/${PROJECT_NAME}:${LOCAL_VOLUMN_PATH}" ${IMAGE_NAME} > .nohup.out 2>&1 &
+    nohup udocker --allow-root run -p "${LOCAL_PORT}:${LOCAL_PORT}" --volume="/content/docker-app/${PROJECT_NAME}:${LOCAL_VOLUMN_PATH}" ${IMAGE_NAME} > .nohup.out 2>&1 &
 else
     echo "Run container in foreground.."
-    udocker --allow-root run -p "${LOCAL_PORT}:${LOCAL_PORT}" --volume="/content/${PROJECT_NAME}:${LOCAL_VOLUMN_PATH}" ${IMAGE_NAME}
+    udocker --allow-root run -p "${LOCAL_PORT}:${LOCAL_PORT}" --volume="/content/docker-app/${PROJECT_NAME}:${LOCAL_VOLUMN_PATH}" ${IMAGE_NAME}
 fi
