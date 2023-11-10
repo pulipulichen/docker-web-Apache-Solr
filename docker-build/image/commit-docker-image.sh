237d1c5ba85188444b24a0c9be2be8742b791919
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-11-11 00:18:11 +0800

    西元2023年11月11日 (週六) 00時18分11秒 CST

--- docker-build/image/commit-docker-image.sh
+++ docker-build/image/commit-docker-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=pudding/docker-web:docker-web-apache-solr-app-20231101-0062
+IMAGE_NAME=pudding/docker-web:docker-web-apache-solr-app-20231101-0063
 
 docker tag docker-web-apache-solr-app ${IMAGE_NAME}
 docker push "${IMAGE_NAME}"
