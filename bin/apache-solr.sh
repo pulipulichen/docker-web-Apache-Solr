c893a8ad37eac5d3a7cc981200e9ff831644696a
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-11-11 00:58:04 +0800

    西元2023年11月11日 (週六) 00時58分04秒 CST

--- bin/apache-solr.sh
+++ bin/apache-solr.sh
@@ -279,7 +279,7 @@ runDockerCompose() {
 
     # openURL "http://127.0.0.1:$PUBLIC_PORT"
     # echo "${cloudflare_url}"
-    if [ "$cloudflare_url" == "false" && project_inited == true ]; then
+    if [ "$cloudflare_url" == "false" ] && [ project_inited == true ]; then
       openURL "http://127.0.0.1:$PUBLIC_PORT"
     else
       openURL "${cloudflare_url}"
