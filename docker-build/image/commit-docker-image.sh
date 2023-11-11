95dd9fcc46d2abaa25a83a585f9da6995e422eaa
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-11-11 14:28:09 +0800

    西元2023年11月11日 (週六) 14時28分09秒 CST

--- docker-build/image/commit-docker-image.sh
+++ docker-build/image/commit-docker-image.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-IMAGE_NAME=pudding/docker-web:docker-web-apache-solr-app-20231101-0068
+new_version=$(date '+%Y%m%d.%H%M%S')
+script_dir=$(dirname "$0")
+yaml_file="$script_dir/docker-compose-template.yml"
 
-docker tag docker-web-apache-solr-app ${IMAGE_NAME}
+# ========
+
+# Get the line starting with "image:"
+image_line=$(awk '/^ *image:/ {print $0}' "$yaml_file")
+
+# Extract the string before the last "-"
+image_config=$(echo "$image_line" | rev | cut -d'-' -f2- | rev)
+
+# Replace the line with the new version
+sed -i "s|^ *image:.*|${image_config}-${new_version}|" "$yaml_file"
+
+# ========
+
+IMAGE_NAME=$(awk '/^ *image:/ {sub(/^ *image: */, ""); sub(/ *$/, ""); print $0}' "$yaml_file")
+
+CONTAINER_NAME=$(awk -F= '/^ *- CONTAINER_NAME=/ {gsub(/ /,"",$2); print $2}' "$yaml_file")
+
+docker tag ${CONTAINER_NAME} ${IMAGE_NAME}
 docker push "${IMAGE_NAME}"
